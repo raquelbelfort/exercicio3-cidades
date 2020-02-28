5f72ccddcec2de661074630e1e816ad22f41cb1f
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-02-28 20:48:48 -0300

    Adição de resolução.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio3_cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Cananéia");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Registro");
+            Console.WriteLine("Miracatu");
+            Console.WriteLine("Itariri");
+            Console.WriteLine("Juquiá");
+            Console.WriteLine("Eldorado");
+            Console.WriteLine("Pedro de Toledo");
+            Console.WriteLine("Sete Barras");
+            Console.WriteLine("Cajati");
+
+
+        }
+    }
+}
